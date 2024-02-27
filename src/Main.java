47ded3f2d5e77d30031b8d0626e1ab36ad528598
Popuskjaer <khasanovtimur2001@gmail.com>
2024-02-27 23:28:57 +0300

    Работа с мапами

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,53 @@
+import java.util.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void task1(){
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        for (Integer num : nums) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
         }
+    }
+    public static void task2() {
+        Set<Integer> set = new HashSet<>();
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        for (Integer num : nums) {
+            if (num % 2 == 0) {
+                set.add(num);
+            }
+        }
+        System.out.println(set);
+    }
+    public static void task3(){
+        List<String> words = new ArrayList<>(Arrays.asList("банан",
+                "арбуз","дыня","банан","ананас","дыня","кешью","картина","мстители","Зачет","кешью"));
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            set.add(word);
+        }
+        System.out.println(set);
+
+    }
+    public static void task4() {
+        List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+        Map<String, Integer> map = new HashMap<>();
+        for (String string : strings) {
+            if (map.containsKey(string)) {
+                map.put(string, map.get(string)+1);
+            }
+            else{
+                map.put(string, 1);
+            }
+        }
+        System.out.println(map.values());
+    }
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
     }
 }
